0aef2631b26ce35adbd98aac639fa6a1075928b1
Kidxcudi <kidxcudi2@gmail.com>
2025-05-21 15:34:45 +0300

    create the bloc logic

---
+++ flutter_lab_assignment_3/lib/logic/album_bloc/album_state.dart
@@ -0,0 +1,19 @@
+import '../../../domain/entities/album.dart';
+
+abstract class AlbumState {}
+
+class AlbumInitial extends AlbumState {}
+
+class AlbumLoading extends AlbumState {}
+
+class AlbumLoaded extends AlbumState {
+  final List<Album> albums;
+
+  AlbumLoaded(this.albums);
+}
+
+class AlbumError extends AlbumState {
+  final String message;
+
+  AlbumError(this.message);
+}
